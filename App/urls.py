8d279934803d6567b5e8340b5e6818d118697b3a
Samyuan <sam.yuancy@gmail.com>
2023-07-21 15:54:39 +0800

    1、修改api包名为Api

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,7 @@
 # urls.py 描述了路由规则
 from .exts import api    # 导入api插件
-from App.apis.HelloResource import HelloResource      # 导入api中创建的视图
-from App.apis.AdminUserResource import AdminUserResource      # 导入api中创建的视图
+from App.Apis.HelloResource import HelloResource      # 导入api中创建的视图
+from App.Apis.AdminUserResource import AdminUserResource      # 导入api中创建的视图
 
 
 api.add_resource(HelloResource, '/hello')   # 添加路由规则，将Hello类视图添加到路由中
