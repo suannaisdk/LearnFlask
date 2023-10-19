2831d582f85e45b02a0e37ed3452d9a69e3cdd4e
Samyuan <sam.yuancy@gmail.com>
2023-10-19 14:55:43 +0800

    1、access_token的过期时间改为15天

--- App/__init__.py
+++ App/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .exts import init_ext
 from .urls import *     # 导入路由规则
-
+from datetime import timedelta
 
 # 初始化App，创建App都在此进行设置
 def create_app():
@@ -12,6 +12,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 禁止对象追踪修改。默认false
     app.config['JWT_SECRET_KEY'] = 'secret'  # 设置jwt加密密钥
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=15)
     # 初始化插件
     init_ext(app)
 
